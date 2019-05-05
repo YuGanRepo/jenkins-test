9f9c604bba57ad65c10f022f6616251c8fb4a16a
丹青阁 <ganyu1016@gmail.com111>
2019-05-05 15:49:23 +0800

    first commit

---
+++ src/main/java/com/glanway/jenkinstest/HelloController.java
@@ -0,0 +1,22 @@
+package com.glanway.jenkinstest;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * .
+ *
+ * @author YuGan
+ * @create 2019-05-05 15:46
+ */
+@RestController
+@RequestMapping("/api")
+public class HelloController {
+
+  @GetMapping("/hello")
+  public String hello() {
+    return "hello";
+  }
+
+}
