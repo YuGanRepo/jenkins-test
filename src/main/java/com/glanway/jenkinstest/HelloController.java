a7230f203f96416b5ea9f0b8628ebd81f8a9c157
丹青阁 <ganyu1016@gmail.com111>
2019-05-06 10:13:51 +0800

    remove method hello3

--- src/main/java/com/glanway/jenkinstest/HelloController.java
+++ src/main/java/com/glanway/jenkinstest/HelloController.java
@@ -19,9 +19,4 @@ public class HelloController {
     return "hello world";
   }
 
-  @GetMapping("/hello2")
-  public String hello2() {
-    return "hello world 2";
-  }
-
 }
--- src/test/java/com/glanway/jenkinstest/JenkinsTestApplicationTests.java
+++ src/test/java/com/glanway/jenkinstest/JenkinsTestApplicationTests.java
@@ -1,16 +1,32 @@
 package com.glanway.jenkinstest;
 
+import static org.hamcrest.Matchers.equalTo;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@AutoConfigureMockMvc
 public class JenkinsTestApplicationTests {
 
+  @Autowired
+  private MockMvc mvc;
+
   @Test
-  public void contextLoads() {
+  public void contextLoads() throws Exception {
+    mvc.perform(MockMvcRequestBuilders.get("/api/hello").accept(MediaType.APPLICATION_JSON_UTF8))
+        .andExpect(status().isOk())
+        .andExpect(content().string(equalTo("hello world")));
   }
 
 }
