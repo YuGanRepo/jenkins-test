7983daf2d289f5ea321a160d3900a0fc7de3bae0
huana <you@example.com>
2019-05-07 22:35:58 +0800

    2

---
+++ build-dev.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+MODULE=$1
+TIME=`date  "+%Y%m%d%H%M"`
+GIT_REVISION=`git log -1 --pretty=format:"%h"`
+IMAGE_PRFIX=hub.yucloud.org/micro-service/${MODULE}
+IMAGE_NAME=${IMAGE_PRFIX}:${TIME}_${GIT_REVISION}
+
+# cd ${MODULE}
+
+docker build -t ${IMAGE_NAME} .
+
+docker rm -f $(docker ps -a |  grep "${IMAGE_PRFIX}*"  | awk '{print $1}')
+
+docker run -d --name ${IMAGE_PRFIX} -p 8090:8080 IMAGE_NAME
+
+# docker push ${IMAGE_NAME}
+
+# docker rmi ${IMAGE_NAME}
+
+# echo "${IMAGE_NAME}" > IMAGE_NAME 
